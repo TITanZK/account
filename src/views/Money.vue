5b4904319ab977156c163af48a407dd2cd82576a
TITanZK <503838819@qq.com>
2020-12-01 21:54:38 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>Money
+  <div class="nav-wrapper">
+    <div class="content">
+      Money
+    </div>
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Money'
-};
+  export default {
+    name: 'Money'
+  };
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
+  .content {
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
