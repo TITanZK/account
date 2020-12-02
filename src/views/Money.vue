c32ae4d9af5f9bade8d865373e648ab41f2923fa
TITanZK <503838819@qq.com>
2020-12-02 14:47:31 +0800

    创建<Layout/>组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Money
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    <p>money.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Money'
-  };
+export default {
+  name: 'Money'
+};
 </script>
-
-<style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-
-  .content {
-    overflow: auto;
-    flex-grow: 1;
-  }
-</style>
