60b0533365c3f14e73db3c9b0532a810e7829ec1
TITanZK <503838819@qq.com>
2020-12-02 16:31:02 +0800

    å差创建<Icon>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require("path")
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, "src/assets/icons")
+
+    config.module
+      .rule("svg-sprite")
+      .test(/\.svg$/)
+      .include.add(dir).end()   //只包含icons目录
+      .use("svg-sprite-loader").loader("svg-sprite-loader").options({extract: false}).end()
+    // .use("svg-loader").loader("svg-loader")
+    // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]}))
+    // .end()
+    config.plugin("svg-sprite").use(require("svg-sprite-loader/plugin"), [{plainSprite: true}])
+    config.module.rule("svg").exclude.add(dir)//其它svg-loader排除icons目录
+  }
 }
