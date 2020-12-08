34200b6b2f1ec1a3e238a0e7bc76c1dd6e10cc3a
TITanZK <503838819@qq.com>
2020-12-08 12:02:48 +0800

    ===模块化===

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+  <label class="notes">
+    <span class="name">备注</span>
+    <input type="text" placeholder="请在此处添加备注">
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    padding-right: 16px;
+    background: transparent;
+    border: none;
+  }
+}
+</style>
