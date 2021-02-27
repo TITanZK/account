ce7160934c96d67c94efe4ba5dbd5ea0142a8f6d
TITanZK <503838819@qq.com>
2021-02-27 22:39:59 +0800

    record 的复制保存到 list

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -58,13 +58,15 @@ export default class NumberPad extends Vue {
   }
 
   ok() {
-    this.$emit('update:value', this.output)
+    this.$emit('update:value', this.output);
+    this.$emit('submit', this.output)
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   .output {
     font-size: 36px;
@@ -74,40 +76,52 @@ export default class NumberPad extends Vue {
     min-height: 72px;
     @extend %innerShadow;
   }
+
   .buttons {
     @extend %clearFix;
+
     > button {
       float: left;
       width: 25%;
       height: 64px;
       background: transparent;
       border: none;
+
       &.ok {
         height: 64*2px;
         float: right;
       }
+
       &.zero {
         width: 25*2%;
       }
+
       $bg: #f2f2f2;
+
       &:nth-child(1) {
         background: $bg;
       }
+
       &:nth-child(2), &:nth-child(5) {
         background: darken($bg, 4%);
       }
+
       &:nth-child(3), &:nth-child(6), &:nth-child(9) {
         background: darken($bg, 4*2%);
       }
+
       &:nth-child(4), &:nth-child(7), &:nth-child(10) {
         background: darken($bg, 4*3%);
       }
+
       &:nth-child(8), &:nth-child(11), &:nth-child(13) {
         background: darken($bg, 4*4%);
       }
+
       &:nth-child(14) {
         background: darken($bg, 4*5%);
       }
+
       &:nth-child(12) {
         background: darken($bg, 4*6%);
       }
