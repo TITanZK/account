e661b99e0a49ea81a4e2042937bacb9ef0946e01
TITanZK <503838819@qq.com>
2021-03-02 14:26:56 +0800

    改造 Notes.vue 成为通用组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,19 +1,22 @@
 <template>
   <label class="notes">
-    <span class="name">备注</span>
+    <span class="name">{{this.fieldName}}</span>
     <input type="text"
            v-model="value"
-           placeholder="请在此处添加备注">
+           :placeholder="this.placeholder">
   </label>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Notes extends Vue {
   value = '';
+
+  @Prop({required: true}) fieldName!: string;
+  @Prop() placeholder?: string;
 
   @Watch('value')
   onValueChanged(value: string) {
