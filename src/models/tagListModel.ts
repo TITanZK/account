ac6030622b1b7e954b138e0f6253bc2e74a3efb4
TITanZK <503838819@qq.com>
2021-03-02 13:04:35 +0800

    将 tag 的 string 改为 {id, name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,11 +1,15 @@
 const localStorageKeyName = 'tagList';
 
+type Tag = {
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated';  //  'success' 成功 ’duplicated‘ 重复
   save: () => void;
-}
+};
 
 const tagListModel: TagListModel = {
   data: [],
@@ -14,8 +18,10 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
-    if (this.data.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push(name);
+    //下面id可后续修改
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id: name, name: name});
     this.save();
     return 'success';
   },
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    编辑标签
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: "EditLabel"
+}
+</script>
+
+<style scoped>
+
+</style>
