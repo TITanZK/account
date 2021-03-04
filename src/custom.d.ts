ef2e6f6f2e4245118dcc1bf214021b8c49d2ad91
TITanZK <503838819@qq.com>
2021-03-04 10:58:09 +0800

    解决 Money 和 Label 标签名不同步问题，使用全局 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,20 @@ type RecordItem = {
   amount: number;
   createdAt?: Date;
 }
+
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => 'success' | 'duplicated';  //  'success' 成功 ’duplicated‘ 重复
+  update: (id: string, name: string) => 'success' | 'notFound' | 'duplicated';
+  remove: (id: string) => boolean;
+  save: () => void;
+}
+
+interface Window {
+  tagList: Tag[];
+}
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -2,19 +2,6 @@ import createId from '@/lib/createId';
 
 const localStorageKeyName = 'tagList';
 
-type Tag = {
-  id: string;
-  name: string;
-}
-type TagListModel = {
-  data: Tag[];
-  fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated';  //  'success' 成功 ’duplicated‘ 重复
-  update: (id: string, name: string) => 'success' | 'notFound' | 'duplicated';
-  remove: (id: string) => boolean;
-  save: () => void;
-};
-
 const tagListModel: TagListModel = {
   data: [],
   fetch() {
