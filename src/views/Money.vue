55a2f36e74025d85db256fdda38ce721f58ebe70
TITanZK <503838819@qq.com>
2021-03-05 23:56:00 +0800

    封装 recordList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,10 +17,9 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
+const recordList = window.recordList;
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad},
@@ -52,14 +51,9 @@ export default class Money extends Vue {
 
   saveRecord(value: string) {
     if (value !== '0' && value !== '0.') {
-      recordListModel.create(this.record);
+      window.createRecord(this.record);
     }
 
-  }
-
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
   }
 }
 </script>
