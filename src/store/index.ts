439b7bde0e4c6f2772b741ced5414b8712e6190a
TITanZK <503838819@qq.com>
2021-03-07 17:36:07 +0800

    Labels.vue 使用 vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -27,9 +27,10 @@ const store = new Vuex.Store({
     fetchTags(state) {
       state.tagList = JSON.parse((window.localStorage.getItem('tagList')) || '[]');
     },
-    findTag(id: string) {
-      return this.tagList.filter(item => item.id === id)[0];
-    },
+
+    // findTag(id: string) {
+    //   return this.tagList.filter(item => item.id === id)[0];
+    // },
 
     createTag(state, name: string) {
       const names = state.tagList.map(item => item.name);
@@ -43,34 +44,34 @@ const store = new Vuex.Store({
       window.alert('添加成功！');
       return 'success';
     },
-    removeTag(id: string) {
-      let index = -1;
-      for (let i = 0; i <= this.tagList.length; i++) {
-        if (this.tagList[i].id === id) {
-          index = i;
-          break;
-        }
-      }
-      this.tagList.splice(index, 1);
-      this.saveTags();
-      return true;
-    },
-    updateTag(id: string, name: string) {
-      const idList = this.tagList.map(item => item.id);
-      if (idList.indexOf(id) >= 0) {
-        const names = this.tagList.map(item => item.name);
-        if (names.indexOf(name) >= 0) {
-          return 'duplicated';
-        } else {
-          const tag = this.tagList.filter(item => item.id === id)[0];
-          tag.id = tag.name = name;//id 存在bug
-          this.saveTags();
-          return 'success';
-        }
-      } else {
-        return 'notFound';
-      }
-    },
+    // removeTag(id: string) {
+    //   let index = -1;
+    //   for (let i = 0; i <= this.tagList.length; i++) {
+    //     if (this.tagList[i].id === id) {
+    //       index = i;
+    //       break;
+    //     }
+    //   }
+    //   this.tagList.splice(index, 1);
+    //   this.saveTags();
+    //   return true;
+    // },
+    // updateTag(id: string, name: string) {
+    //   const idList = this.tagList.map(item => item.id);
+    //   if (idList.indexOf(id) >= 0) {
+    //     const names = this.tagList.map(item => item.name);
+    //     if (names.indexOf(name) >= 0) {
+    //       return 'duplicated';
+    //     } else {
+    //       const tag = this.tagList.filter(item => item.id === id)[0];
+    //       tag.id = tag.name = name;//id 存在bug
+    //       this.saveTags();
+    //       return 'success';
+    //     }
+    //   } else {
+    //     return 'notFound';
+    //   }
+    // },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
     }
