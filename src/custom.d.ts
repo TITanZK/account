293b771ace06b818c0c70a9b16f5d84dd29c3432
TITanZK <503838819@qq.com>
2021-03-07 09:43:35 +0800

    模块化 store , 删除了 models

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -10,11 +10,3 @@ type Tag = {
   id: string;
   name: string;
 }
-type TagListModel = {
-  data: Tag[];
-  fetch: () => Tag[];
-  create: (name: string) => 'success' | 'duplicated';  //  'success' 成功 ’duplicated‘ 重复
-  update: (id: string, name: string) => 'success' | 'notFound' | 'duplicated';
-  remove: (id: string) => boolean;
-  save: () => void;
-}
