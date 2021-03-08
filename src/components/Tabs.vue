f18b936f8cd097f4c5d5b31a7c0ffd4830750e17
TITanZK <503838819@qq.com>
2021-03-08 17:13:38 +0800

    更新 CSS

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
-  <ul class="tabs">
+  <ul class="tabs" :class="{[classPrefix+'-tabs']:classPrefix}">
     <li v-for="item in dataSource" :key="item.value"
         @click="select(item)"
+        class="tabs-item"
         :class="{ selected:value === item.value, [classPrefix + '-tabs-item']: classPrefix }"
     >
       {{ item.text }}
@@ -34,7 +35,7 @@ export default class Tabs extends Vue {
   text-align: center;
   font-size: 24px;
 
-  > li {
+  &-item {
     width: 50%;
     height: 64px;
     display: flex;
