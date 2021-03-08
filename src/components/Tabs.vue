63e8f5fff4e951286c6c6ab0697c4c8e401c2ef4
TITanZK <503838819@qq.com>
2021-03-08 15:26:23 +0800

    抽离 Tabs.vue 成为切换组件全局使用

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,10 +1,10 @@
 <template>
-  <ul class="types">
-    <li :class="{selected:value === '-', [classPrefix + '-item']: classPrefix}"
-        @click="selectType('-')">支出
-    </li>
-    <li :class="{selected:value === '+', [classPrefix + '-item']: classPrefix}"
-        @click="selectType('+')">收入
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value"
+        @click="select(item)"
+        :class="{ selected:value === item.value, [classPrefix + '-tabs-item']: classPrefix }"
+    >
+      {{ item.text }}
     </li>
   </ul>
 </template>
@@ -13,22 +13,22 @@
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 
+type DataSourceItem = { text: string; value: string }
+
 @Component
-export default class Types extends Vue {
+export default class Tabs extends Vue {
+  @Prop({required: true, type: Array}) dataSource!: DataSourceItem[];
   @Prop(String) readonly value!: string;
   @Prop(String) classPrefix?: string;
 
-  selectType(type: string) {
-    if (type !== '-' && type !== '+') {
-      throw new Error('type is unknown');
-    }
-    this.$emit('update:value', type);
+  select(item: DataSourceItem) {
+    this.$emit('update:value', item.value);
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.types {
+<style scoped lang="scss">
+.tabs {
   background: #c4c4c4;
   display: flex;
   text-align: center;
