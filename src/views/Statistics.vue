de3a94ea172124756c0aab61506d54ab55239dad
TITanZK <503838819@qq.com>
2021-03-09 11:20:00 +0800

    提取 RootState 类型到全局 ；修改 createdAt 类型为 string ；Statistics.vue 数据粗展示

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,16 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="interList" :value.sync="interval"/>
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3>{{group.title}}</h3>
+        <ol>
+          <li v-for="(item, index) in group.items" :key="index">
+            {{ item.amount }}
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -16,6 +26,26 @@ import typeList from '@/constants/typeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = { title: string; items: RecordItem[] }
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < this.recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   interList = interList;
