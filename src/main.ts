5213a265b1e463793a7472581512bef2b734a793
TITanZK <503838819@qq.com>
2021-03-20 14:45:44 +0800

    pc提示优化

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,15 @@ window.onload = function () {
     window.scrollTo(0, 200);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 769) {
+  window.alert('请使用手机或平板打开页面，以保证浏览效果！');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform='translate(-50%,-50%)'
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img);
+}
